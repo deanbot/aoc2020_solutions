d0be5e5c5db7b8ffa5e6c1a2d4d6021882a4af42
Dean Verleger <deanverleger@gmail.com>
2020-12-01 20:51:15 -0600

    initial commit with day1 solution

---
+++ README.md
@@ -0,0 +1,3 @@
+# aoc2020_solutions
+
+Advent of Code 2020 solutions via dart. This is mostly a playground to experiment with dart language features.
---
+++ android/app/src/main/kotlin/com/example/manage_state/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aoc2020_solutions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
