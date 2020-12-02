1f73c9d2c1fe2ea65c0e3aeffc3dfeb8a6a3076c
Dean Verleger <deanverleger@gmail.com>
2020-12-02 10:36:02 -0600

    add day 2 part 1 solution

--- lib/src/day2/solution.dart
+++ lib/src/day2/solution.dart
@@ -2,9 +2,47 @@ import 'package:aoc2020_solutions/src/adventSolution.dart';
 
 import 'inputs.dart';
 
-List<String> _parseInputs(String inputs) =>
-    inputs.split("\n").where((element) => element.isNotEmpty).toList();
-List<String> _passwords = _parseInputs(inputsDay2);
+// parse input line i.e.: "1-3 a: abcde" to get password record items
+List<PasswordRecord> _parseInputs(String inputs) => inputs
+        .split("\n")
+        .where((element) => element.isNotEmpty)
+        .map<PasswordRecord>((e) {
+      var pieces = e.split(": "),
+          policyPieces = pieces[0].split(' '),
+          range = policyPieces[0].split('-');
+      return PasswordRecord(
+        password: pieces[1],
+        targetLetter: policyPieces[1],
+        targetLowerBounds: int.parse(range[0]),
+        targetUpperBounds: int.parse(range[1]),
+      );
+    }).toList();
+List<PasswordRecord> _passwords = _parseInputs(inputsDay2);
+
+class PasswordRecord {
+  final String _password;
+  final String _targetLetter;
+  final int _targetLowerBounds;
+  final int _targetUpperBounds;
+  PasswordRecord(
+      {String password,
+      String targetLetter,
+      int targetLowerBounds,
+      int targetUpperBounds})
+      : _password = password,
+        _targetLetter = targetLetter,
+        _targetLowerBounds = targetLowerBounds,
+        _targetUpperBounds = targetUpperBounds;
+
+  bool get isValid {
+    final match = RegExp(_targetLetter),
+      targetCount = match.allMatches(_password).length;
+    return targetCount >= _targetLowerBounds &&
+        targetCount <= _targetUpperBounds;
+  }
+
+  String get password => this._password;
+}
 
 abstract class _Day2Solution extends AdventSolution {
   _Day2Solution(name) : super(2, name);
@@ -14,7 +52,8 @@ class SolutionA extends _Day2Solution {
   SolutionA() : super("A");
 
   String getSolution() {
-    return "";
+    var validCount = _passwords.where((element) => element.isValid).length;
+    return validCount.toString();
   }
 }
 
