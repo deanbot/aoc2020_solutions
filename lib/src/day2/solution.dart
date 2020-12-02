2b1043422d39c7136fde7fdfcb77ab0d8cb05866
Dean Verleger <deanverleger@gmail.com>
2020-12-02 11:03:32 -0600

    add second solution

--- lib/src/day2/solution.dart
+++ lib/src/day2/solution.dart
@@ -2,7 +2,30 @@ import 'package:aoc2020_solutions/src/adventSolution.dart';
 
 import 'inputs.dart';
 
-// parse input line i.e.: "1-3 a: abcde" to get password record items
+abstract class _Day2Solution extends AdventSolution {
+  _Day2Solution(name) : super(2, name);
+}
+
+class SolutionA extends _Day2Solution {
+  SolutionA() : super("A");
+
+  String getSolution() {
+    var validCount =
+        _passwords.where((element) => element.isValidLegacy).length;
+    return validCount.toString();
+  }
+}
+
+class SolutionB extends _Day2Solution {
+  SolutionB() : super("B");
+
+  String getSolution() {
+    var validCount = _passwords.where((element) => element.isValid).length;
+    return validCount.toString();
+  }
+}
+
+// parse input lines i.e. of: "1-3 a: abcde" to get password record items
 List<PasswordRecord> _parseInputs(String inputs) => inputs
         .split("\n")
         .where((element) => element.isNotEmpty)
@@ -12,55 +35,38 @@ List<PasswordRecord> _parseInputs(String inputs) => inputs
           range = policyPieces[0].split('-');
       return PasswordRecord(
         password: pieces[1],
-        targetLetter: policyPieces[1],
-        targetLowerBounds: int.parse(range[0]),
-        targetUpperBounds: int.parse(range[1]),
+        searchCharacter: policyPieces[1],
+        indexLower: int.parse(range[0]),
+        indexUpper: int.parse(range[1]),
       );
     }).toList();
 List<PasswordRecord> _passwords = _parseInputs(inputsDay2);
 
 class PasswordRecord {
   final String _password;
-  final String _targetLetter;
-  final int _targetLowerBounds;
-  final int _targetUpperBounds;
+  final String _searchCharacter;
+  final int _indexLower;
+  final int _indexUpper;
   PasswordRecord(
-      {String password,
-      String targetLetter,
-      int targetLowerBounds,
-      int targetUpperBounds})
+      {String password, String searchCharacter, int indexLower, int indexUpper})
       : _password = password,
-        _targetLetter = targetLetter,
-        _targetLowerBounds = targetLowerBounds,
-        _targetUpperBounds = targetUpperBounds;
+        _searchCharacter = searchCharacter,
+        _indexLower = indexLower,
+        _indexUpper = indexUpper;
 
+  // total search occurances fall within min and max as defined by lower and upper index
+  bool get isValidLegacy {
+    final match = RegExp(_searchCharacter),
+        searchCount = match.allMatches(_password).length;
+    return searchCount >= _indexLower && searchCount <= _indexUpper;
+  }
+
+  // search should occur in either index (1-based) but not both
   bool get isValid {
-    final match = RegExp(_targetLetter),
-      targetCount = match.allMatches(_password).length;
-    return targetCount >= _targetLowerBounds &&
-        targetCount <= _targetUpperBounds;
+    var t1Valid = _password[_indexLower - 1] == _searchCharacter,
+        t2Valid = _password[_indexUpper - 1] == _searchCharacter;
+    return (t1Valid || t2Valid) && !(t1Valid && t2Valid);
   }
 
   String get password => this._password;
-}
-
-abstract class _Day2Solution extends AdventSolution {
-  _Day2Solution(name) : super(2, name);
-}
-
-class SolutionA extends _Day2Solution {
-  SolutionA() : super("A");
-
-  String getSolution() {
-    var validCount = _passwords.where((element) => element.isValid).length;
-    return validCount.toString();
-  }
-}
-
-class SolutionB extends _Day2Solution {
-  SolutionB() : super("B");
-
-  String getSolution() {
-    return "";
-  }
 }
