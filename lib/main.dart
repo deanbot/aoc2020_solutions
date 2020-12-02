e429c471c1483b79da1b3f4adfd45f8fe5e60342
Dean Verleger <deanverleger@gmail.com>
2020-12-01 21:04:35 -0600

    remove tutorial content

--- lib/main.dart
+++ lib/main.dart
@@ -39,66 +39,10 @@ class AdventDay1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return GridView.count(
+      primary: false,
+      crossAxisCount: 2,
       children: [Text(solutionA.toString()), Text(solutionB.toString())],
     );
   }
 }
-
-/*
-
-const List<String> urls = [
-  "https://www.ultraboardgames.com/the-taverns-of-tiefenthal/gfx/game11.jpg",
-  "https://www.ultraboardgames.com/img/slideshow/the-taverns-of-tiefenthal.jpg",
-  "https://www.ultraboardgames.com/the-taverns-of-tiefenthal/gfx/game2.jpg",
-  "https://www.ultraboardgames.com/the-taverns-of-tiefenthal/gfx/components.jpg",
-];
-
-class App extends StatefulWidget {
-  @override
-  AppState createState() => AppState();
-}
-
-class AppState extends State<App> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Photo Viewer',
-        home: GalleryPage(
-          title: "Image Gallery",
-          urls: urls,
-        ));
-  }
-}
-
-class GalleryPage extends StatelessWidget {
-  final String title;
-  final List<String> urls;
-
-  GalleryPage({this.title, this.urls});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text(this.title)),
-        body: GridView.count(
-          primary: false,
-          crossAxisCount: 2,
-          children: List.of(urls.map((url) => Photo(url: url))),
-        ));
-  }
-}
-
-class Photo extends StatelessWidget {
-  final String url;
-
-  Photo({this.url});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(top: 10, left: 5, right: 5),
-        child: Image.network(url));
-  }
-}
-*/
