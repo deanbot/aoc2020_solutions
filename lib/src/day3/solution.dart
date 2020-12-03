bc06e314eef55b8008ded38a9d6aa58ff3a1e594
Dean Verleger <deanverleger@gmail.com>
2020-12-03 10:04:02 -0600

    add day 3 scaffolding

---
+++ lib/src/day3/solution.dart
@@ -0,0 +1,23 @@
+import 'package:aoc2020_solutions/src/adventSolution.dart';
+
+import 'inputs.dart';
+
+abstract class _Day3Solution extends AdventSolution {
+  _Day3Solution(name) : super(3, name);
+}
+
+class SolutionA extends _Day3Solution {
+  SolutionA() : super("A");
+
+  String getSolution() {
+    return "";
+  }
+}
+
+class SolutionB extends _Day3Solution {
+  SolutionB() : super("B");
+
+  String getSolution() {
+    return "";
+  }
+}
