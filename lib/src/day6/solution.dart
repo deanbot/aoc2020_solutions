5d4a4ff397105980d806e1efdd24a6539c69dea5
Dean Verleger <deanverleger@gmail.com>
2020-12-06 17:36:04 -0600

    add day 6 solutions

--- lib/src/day6/solution.dart
+++ lib/src/day6/solution.dart
@@ -2,9 +2,33 @@ import 'package:aoc2020_solutions/src/adventSolution.dart';
 
 import 'inputs.dart';
 
-List<String> _parseInputs(String inputs) =>
-    inputs.split('\n').where((element) => element.isNotEmpty);
-List<String> _inputs = _parseInputs(inputsDay6);
+List<Group> _parseInputs(String inputs) => inputs
+    .split('\n\n')
+    .where((element) => element.isNotEmpty)
+    .map<Group>((e) => Group.fromString(e))
+    .toList();
+List<Group> _inputs = _parseInputs(inputsDay6);
+
+// custom declarations form group
+class Group {
+  final List<String> uniqueAnswers;
+  List<String> unionAnswers = [];
+  Group.fromString(String input)
+      // flatten and remove duplicates
+      : uniqueAnswers =
+            input.split('\n').expand((e) => e.split('')).toSet().toList() {
+    // add each char in each line to union answers if char is present in every line then remove duplicates
+    List<String> groupAnswers = input.split('\n');
+    groupAnswers.forEach((lineAnswers) {
+      lineAnswers.split('').forEach((character) {
+        if (groupAnswers.every((line) => line.indexOf(character) != -1)) {
+          unionAnswers.add(character);
+        }
+      });
+    });
+    unionAnswers = unionAnswers.toSet().toList();
+  }
+}
 
 abstract class _Day6Solution extends AdventSolution {
   _Day6Solution(name) : super(6, name);
@@ -14,7 +38,10 @@ class SolutionA extends _Day6Solution {
   SolutionA() : super('A');
 
   String getSolution() {
-    return _inputs.length.toString();
+    return _inputs
+        .map<int>((e) => e.uniqueAnswers.length)
+        .fold(0, (previousValue, e) => previousValue + e)
+        .toString();
   }
 }
 
@@ -22,7 +49,9 @@ class SolutionB extends _Day6Solution {
   SolutionB() : super('B');
 
   String getSolution() {
-    return "";
+    return _inputs
+        .map<int>((e) => e.unionAnswers.length)
+        .fold(0, (previousValue, e) => previousValue + e)
+        .toString();
   }
 }
-
--- lib/src/solutions.dart
+++ lib/src/solutions.dart
@@ -3,7 +3,7 @@ import 'day2/solution.dart' as day2;
 import 'day3/solution.dart' as day3;
 import 'day4/solution.dart' as day4;
 import 'day5/solution.dart' as day5;
-// import 'day6/solution.dart' as day6;
+import 'day6/solution.dart' as day6;
 // import 'day7/solution.dart' as day7;
 // import 'day8/solution.dart' as day8;
 // import 'day9/solution.dart' as day9;
@@ -34,8 +34,8 @@ final List<Object> solutions = [
   day4.SolutionB(),
   day5.SolutionA(),
   day5.SolutionB(),
-  // day6.SolutionA(),
-  // day6.SolutionB(),
+  day6.SolutionA(),
+  day6.SolutionB(),
   // day7.SolutionA(),
   // day7.SolutionB(),
   // day8.SolutionA(),
